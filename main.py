4d8f20f9e1c3da544042b8e6d5af9c4c71b04559
yaoenliu <peterliu1060@gmail.com>
2024-08-20 18:35:27 +0800

    change arg

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import subprocess
 import xml.etree.ElementTree as ET
 import ffmpeg
 import os
+import sys
 
-filename = "test.mka"
-output_path = filename.split(".")[0]
+if len(sys.argv) < 2:
+    print("Usage: python main.py <filename>")
+    sys.exit()
+
+filepath = sys.argv[1]
+filename = filepath[filepath.rfind("\\")+1:filepath.rfind(".")]
+if filename == "":
+    filename = filepath.split(".")[0]
+
 # make directory
-os.makedirs (output_path, exist_ok=True)
+os.makedirs (filename, exist_ok=True)
 
-
-
-result = subprocess.run(['mkvextract', 'chapters', filename], stdout=subprocess.PIPE, text=True, encoding='UTF-8')
+result = subprocess.run(['mkvextract', 'chapters', filepath], stdout=subprocess.PIPE, text=True, encoding='UTF-8')
 
 text = result.stdout
 root = ET.fromstring(text)
@@ -20,6 +26,5 @@ for ele in root.iter('ChapterAtom'):
     stime = ele.find('ChapterTimeStart').text
     etime = ele.find('ChapterTimeEnd').text
     # export to ffmpeg as flac
-    ffmpeg.input("test.mka", ss=stime, to=etime).output(output_path+"\\"+str(idx).zfill(2)+". "+name+".flac").run()
-    idx += 1  
-    
+    ffmpeg.input("test.mka", ss=stime, to=etime).output(filename+"\\"+str(idx).zfill(2)+". "+name+".flac").run()
+    idx += 1
